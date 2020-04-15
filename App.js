7705b9644ab65417a4ac4fc100ffc81f6205c6eb
li <li@chaoquns-MacBook-Air.local>
2020-04-15 10:23:05 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+import {Button} from 'antd-mobile';
+
+
+const App = () => {
+   return (
+    <View><Button><Text>CustomButton</Text></Button></View>
+   );
+};
+export default App;
